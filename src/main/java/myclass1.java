ed5c212ea7fa93610786d18336da640fc83c768f
vibhav2000 <46700974+vibhav2000@users.noreply.github.com>
2020-10-06 12:05:13 +0530

    added files

---
+++ src/main/java/myclass1.java
@@ -0,0 +1,18 @@
+
+public class myclass1 {
+	private int a1 = 20;
+	
+	public static void main(String[] args) {
+		int a2=30;
+		
+		System.out.println("hello world");
+		
+		int b = a2*10;
+		
+		
+		System.out.println(b);
+		
+
+	}
+
+}
